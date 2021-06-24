7b9d5686e0f27a06996ee9f22a2ed0cf5e2a1d7d
Lawa448 <83343403+Lawa448@users.noreply.github.com>
2021-06-24 16:23:31 +1000

    First commitment

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour
+
+{
+    // Start is called before the first frame update
+    public CharacterController controller;
+
+    public float speed = 20f;
+
+    public float gravity = -9.8f;
+
+    public float jumpHeight = 30f;
+
+    Vector3 velocity;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+		velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+
+        if (Input.GetKeyDown ("space")){
+         velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+		void OnControllerColliderHit(ControllerColliderHit hit){
+
+		if(hit.gameObject.tag == "Enemy"){
+			Debug.Log("Respawn");
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+		}
+		}
+    }
+}
---
+++ Assets/RangeWeapon.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangeWeapon : MonoBehaviour
+{
+
+public Camera fpsCam;
+public float range = 100f;
+    public float damage = 10f;
+    public float impactForce = 200f;
+
+    public ParticleSystem hitParticles;
+
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1")){
+
+        Shoot();
+
+        }
+
+    }
+    void Shoot(){
+
+        RaycastHit hit;
+        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range)){
+            Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+
+            if(target != null){
+                target.TakeDamage(damage);
+            }
+                Instantiate(hitParticles, hit.point, Quaternion.LookRotation(hit.normal));
+
+            if(hit.rigidbody != null){
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+
+            }
+
+        }
+    
+    }
+}
